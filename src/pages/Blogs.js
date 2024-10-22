4185d8fa180df140693746a3045b01362e9e820c
ulhas1102 <ulhasjohari269@gmail.com>
2024-10-22 17:13:03 +0530

    updated blog page comment

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -5,7 +5,8 @@ const Blogs = () => {
     <>
       <div className="container">
         <div className="row">
-          <div className="col-md-12 mt-md-5">
+          <div className="col-md-12 mt-md-5 mt-5">
+            {/* this is blog page container */}
             <h1 className="text-center">Welcome To Blogs Page</h1>
           </div>
         </div>
